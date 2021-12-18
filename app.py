e5f4df76a25eb1370c8fe23f63909cd02f2b0c3f
Sophia Yang <sophimyang@gmail.com>
2021-12-18 14:14:39 -0600

    go back to one app and simplify the app using pn.bind

--- app.py
+++ app.py
@@ -2,23 +2,19 @@ from bokeh.sampledata.autompg import autompg_clean as df
 import hvplot.pandas
 import panel as pn
 
-bootstrap = pn.template.BootstrapTemplate(title='Example')
-select_origin = pn.widgets.Select(
-    options=["North America", "Asia", "Europe"], name="origin"
-)
-select_cyl = pn.widgets.IntSlider(name="cyl", start=4, end=8, step=1)
-
-
-@pn.depends(select_origin, select_cyl)
-def exp_plot(select_origin, select_cyl):
+def exp_plot(origin, cylinders):
     return (
-        df[(df.origin == select_origin) & (df.cyl == select_cyl)]
+        df[(df.origin == origin) & (df.cyl == cylinders)]
         .sort_values(by="mpg")
         .hvplot(x="mpg", y="hp")
     )
 
-bootstrap.sidebar.append(select_origin)
-bootstrap.sidebar.append(select_cyl)
-bootstrap.main.append(exp_plot)
+continents = ["North America", "Asia", "Europe"]
+origin = pn.widgets.Select(options=continents, name="Origin")
+cylinders = pn.widgets.IntSlider(name="Cylinders", start=4, end=8, step=1)
+template = pn.template.BootstrapTemplate(title="Example")
 
-bootstrap.servable()
+template.sidebar.append(origin)
+template.sidebar.append(cylinders)
+template.main.append(pn.bind(exp_plot, origin, cylinders))
+template.servable()
